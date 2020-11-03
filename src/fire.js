3022f25978b25e9924cc459a2b1e0b26eee0f5b6
Domingos Netto <41163007+NettoBkp@users.noreply.github.com>
2020-11-03 04:10:16 -0300

    t

--- src/fire.js
+++ src/fire.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
   var firebaseConfig = {
-    apiKey: "AIzaSyDXQNAJtErwRQ8a7zQN38F85pY89fcWACI", 
-    authDomain: "astiweb-a5d31.firebaseapp.com",
-    databaseURL: "https://astiweb-a5d31.firebaseio.com",
-    projectId: "astiweb-a5d31",
-    storageBucket: "astiweb-a5d31.appspot.com",
-    messagingSenderId: "161192035598",
-    appId: "1:161192035598:web:fe8bbf874896ebafa7c35f"
+    
   };
   // Initialize Firebase
 const fire =  firebase.initializeApp(firebaseConfig);
