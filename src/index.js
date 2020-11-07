04b0c21c46523817dc844e164bdd72db11b4ceeb
Domingos Netto <41163007+NettoBkp@users.noreply.github.com>
2020-11-06 23:42:12 -0300

    Sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+Sentry.init({
+  dsn: "https://c3bb49f3c53549ce903f65141dbb9632@o473118.ingest.sentry.io/5507733",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
