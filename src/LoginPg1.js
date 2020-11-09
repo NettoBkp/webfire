849f3ca18218c0895e2a8613df2cbabd34dfc07b
Domingos Netto <41163007+NettoBkp@users.noreply.github.com>
2020-11-09 02:22:19 -0300

    anchor

--- src/LoginPg1.js
+++ src/LoginPg1.js
@@ -37,16 +37,16 @@ const LoginPg = (props) => {
                                 <input type="submit" value="Login" className="btn solid" />
                                 <p className="social-text">Or Sign in with social platforms</p>
                                 <div className="social-media">
-                                    <a href="#" className="social-icon">
+                                    <a href="#a" className="social-icon">
                                         <i className="fab fa-facebook-f"></i>
                                     </a>
-                                    <a href="#" className="social-icon">
+                                    <a href="#a" className="social-icon">
                                         <i className="fab fa-twitter"></i>
                                     </a>
-                                    <a href="#" className="social-icon">
+                                    <a href="#a" className="social-icon">
                                         <i className="fab fa-google"></i>
                                     </a>
-                                    <a href="#" className="social-icon">
+                                    <a href="#a" className="social-icon">
                                         <i className="fab fa-linkedin-in"></i>
                                     </a>
                                 </div>
